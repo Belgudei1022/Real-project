ee36a472060a0fd70bc22ea22e1f7cbb2a8c1ba1
Belgudei1022 <belgudei379@gmail.com>
2025-01-13 01:10:44 +0800

    bek

---
+++ src/features/cart/pages/cart.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "../../../shared/components/Nav/head";
+import Category from "../../../shared/components/Nav/category";
+
+const Cart = () => {
+  return (
+    <div className="w-full h-fit">
+      <Head />
+      <Category />
+      <div className="w-full h-full flex flex-col">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,14 @@ import Home from "./features/home/pages/Home.jsx";
 import SingleProduct from "./features/singleProduct/pages/singleProduct.jsx";
 import LoginForm from "./features/auth/pages/login-form.jsx";
 import UserInformation from "./features/userInformation/pages/userInformation.jsx";
+import Cart from "./features/cart/pages/cart.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "product/:id", element: <SingleProduct /> },
   { path: "login", element: <LoginForm /> },
   { path: "user", element: <UserInformation /> },
+  { path: "cart", element: <Cart /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
